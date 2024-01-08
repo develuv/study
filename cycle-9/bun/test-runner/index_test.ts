3285a9a492af1b916ea20b7909c9533590508daa
sungkwang kim <sungkwang@wantedlab.com>
2024-01-08 22:23:37 +0900

    feat: --bail

---
+++ cycle-9/bun/test-runner/index_test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "bun:test";
+
+test("kim jae sub", () => {
+  expect(2 + 2).toBe(2);
+});
+
+
+test("yang sang hoon", () => {
+  expect(2 + 2).toBe(2);
+});
+
+
+test("lee dong ju", () => {
+  expect(2 + 2).toBe(2);
+});
+
+test("son jin young", () => {
+  expect(2 + 2).toBe(4);
+});
